bca22a1572e3f8f1abdffa2f977f2856a3acb197
John Twyman <john.twyman@greens.org.au>
2020-12-01 12:50:52 +1100

    Refactor for performance and non-indexed cache tables

--- CRM/CivirulesConditions/Utils/GroupContact.php
+++ CRM/CivirulesConditions/Utils/GroupContact.php
@@ -19,6 +19,24 @@ class CRM_CivirulesConditions_Utils_GroupContact {
     ) {
       return FALSE;
     }
+    $query = "SELECT saved_search_id FROM `civicrm_group` WHERE group_id=%1";
+    $queryParams = [
+      1 => [$group_id, 'Positive'],
+    ];
+    if (CRM_Core_DAO::singleValueQuery($query, $queryParams, FALSE)) {
+      // @fixme We really need a "is in smartgroup" API!!
+      // If the groups are smartgroups (saved searches) they may be out of date.
+      // This triggers a check (and rebuild if necessary).
+      \CRM_Contact_BAO_GroupContactCache::check($group_id);
+      $query = "SELECT group_id FROM `civicrm_group_contact_cache` WHERE group_id=%1 AND contact_id=%2";
+      $queryParams = [
+        1 => [$group_id, 'Positive'],
+        2 => [$contact_id, 'Positive'],
+      ];
+      if (CRM_Core_DAO::singleValueQuery($query, $queryParams, FALSE)) {
+        return TRUE;
+      }
+    }
     try {
       $groupContactCount = civicrm_api3('GroupContact', 'getcount', [
         'group_id' => $group_id,
@@ -29,20 +47,6 @@ class CRM_CivirulesConditions_Utils_GroupContact {
       return FALSE;
     }
     if ($groupContactCount > 0) {
-      return TRUE;
-    }
-
-    // @fixme We really need a "is in smartgroup" API!!
-    // If the groups are smartgroups (saved searches) they may be out of date.
-    // This triggers a check (and rebuild if necessary).
-    \CRM_Contact_BAO_GroupContactCache::check($group_id);
-
-    $query = "SELECT id FROM `civicrm_group_contact_cache` WHERE group_id=%1 AND contact_id=%2";
-    $queryParams = [
-      1 => [$group_id, 'Positive'],
-      2 => [$contact_id, 'Positive'],
-    ];
-    if (CRM_Core_DAO::singleValueQuery($query, $queryParams, FALSE)) {
       return TRUE;
     }
 
