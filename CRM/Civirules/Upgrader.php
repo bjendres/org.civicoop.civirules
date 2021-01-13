5c9e2c4e27c9546abc63844f31c05edae3e3a154
David Hayes <dhayes@lalgbtcenter.org>
2021-01-12 19:12:56 -0800

    ensure upgrade task returns true

--- CRM/Civirules/Upgrader.php
+++ CRM/Civirules/Upgrader.php
@@ -759,6 +759,7 @@ class CRM_Civirules_Upgrader extends CRM_Civirules_Upgrader_Base {
   public function upgrade_2062() {
     $this->ctx->log->info('Applying update 2062 - Add generic date comparison class.');
     CRM_Civirules_Utils_Upgrader::insertConditionsFromJson($this->extensionDir . DIRECTORY_SEPARATOR . 'sql/conditions.json');
+    return TRUE;
   }
 
   public function upgrade_2063() {
