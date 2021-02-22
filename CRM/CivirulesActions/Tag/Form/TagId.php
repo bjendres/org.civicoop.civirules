22e782d87b942cf91a21c69fee07d4c00ed7107f
maynardsmith <tony.maynard-smith@ntlworld.com>
2021-02-22 16:49:41 +0000

    Fix problems with HasTag Condition and Add/Remove Tag Actions

--- CRM/CivirulesActions/Tag/Form/TagId.php
+++ CRM/CivirulesActions/Tag/Form/TagId.php
@@ -19,6 +19,7 @@ class CRM_CivirulesActions_Tag_Form_TagId extends CRM_CivirulesActions_Form_Form
     $bao = new CRM_Core_BAO_Tag();
     switch ($this->trigger->object_name) {
       case 'Contact':
+      case 'Membership':
         $tableName = 'civicrm_contact';
         break;
 
